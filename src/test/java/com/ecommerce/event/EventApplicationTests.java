912d3b79a96f9f0b2b3a59980d88115d2a798cf8
oeun42 <oeunzzang@naver.com>
2024-09-20 19:17:26 +0900

    init

---
+++ src/test/java/com/ecommerce/event/EventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
